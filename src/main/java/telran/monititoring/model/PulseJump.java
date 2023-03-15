6c5a099342a12efc7aaac1432451c4e398f1062f
Mik <m.shac86@gmail.com>
2023-03-15 16:18:58 +0200

    HW_74

--- src/main/java/telran/monititoring/model/PulseJump.java
+++ src/main/java/telran/monititoring/model/PulseJump.java
@@ -22,5 +22,34 @@ public class PulseJump {
 		return "PulseJump [patientId=" + patientId + ", previousValue=" + previousValue + ", currentValue="
 				+ currentValue + ", timestamp=" + timestamp + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + currentValue;
+		result = prime * result + (int) (patientId ^ (patientId >>> 32));
+		result = prime * result + previousValue;
+		result = prime * result + (int) (timestamp ^ (timestamp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PulseJump other = (PulseJump) obj;
+		if (currentValue != other.currentValue)
+			return false;
+		if (patientId != other.patientId)
+			return false;
+		if (previousValue != other.previousValue)
+			return false;
+		return true;
+	}
 	
 }
--- src/main/java/telran/monititoring/model/PulseProbe.java
+++ src/main/java/telran/monititoring/model/PulseProbe.java
@@ -20,6 +20,33 @@ public class PulseProbe {
 	@Override
 	public String toString() {
 		return "PulseProbe#" + sequenceNumber + " [patientID=" + patientID + ", timestamp=" + timestamp + ", value=" + value + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (patientID ^ (patientID >>> 32));
+		result = prime * result + (int) (sequenceNumber ^ (sequenceNumber >>> 32));
+		result = prime * result + (int) (timestamp ^ (timestamp >>> 32));
+		result = prime * result + value;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PulseProbe other = (PulseProbe) obj;
+		if (patientID != other.patientID)
+			return false;
+		if (value != other.value)
+			return false;
+		return true;
 	} 		
 	
 }
